59a9e56abb86969d8fad9cd167ee4642a890b6ee
Matthew Gibbons <mhgibbons@me.com>
2025-10-10 05:31:49 +0000

    Better typing

--- src/lib/components/hast/hast.svelte
+++ src/lib/components/hast/hast.svelte
@@ -7,7 +7,7 @@
 		ast,
 		components = {},
 		...rest
-	}: { ast: import('hast').Root } & ComponentProps<typeof Unist> = $props();
+	}: { ast: import('hast').Root } & Omit<ComponentProps<typeof Unist>, 'ast'> = $props();
 </script>
 
 <Unist {ast} components={{ ...defaultComponents, ...components }} {...rest} />
