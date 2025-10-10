4a60a49afcf5559cfc17cebddc08108f8c54563f
Matthew Gibbons <mhgibbons@me.com>
2025-10-10 05:32:06 +0000

    Build

--- dist/components/hast/hast.svelte.d.ts
+++ dist/components/hast/hast.svelte.d.ts
@@ -2,7 +2,7 @@ import { Unist } from '@accuser/svelte-unist';
 import type { ComponentProps } from 'svelte';
 type $$ComponentProps = {
     ast: import('hast').Root;
-} & ComponentProps<typeof Unist>;
+} & Omit<ComponentProps<typeof Unist>, 'ast'>;
 declare const Hast: import("svelte").Component<$$ComponentProps, {}, "">;
 type Hast = ReturnType<typeof Hast>;
 export default Hast;
